6fb9fc3931c60c60db0fc66207cd65a8c8eec03f
Jeremy Field <jdf@jdf.id.au>
2023-09-25 15:19:37 +1000

    ui (repl-frame vis only at the moment)

---
+++ src/comfort/ui.clj
@@ -0,0 +1,85 @@
+(ns comfort.ui
+  "Simple no-dependency Swing vis infra.
+   No GUI interactivity yet..."
+  (:import (java.time LocalDate LocalDateTime)
+           (java.time.temporal ChronoUnit)
+           (java.time.format DateTimeFormatter)
+           (javax.swing JFrame JPanel JComponent)
+           (java.awt
+             Frame
+             Graphics Graphics2D
+             Dimension
+             Color BasicStroke
+             Rectangle)
+           (java.awt.event
+             WindowStateListener
+             WindowEvent
+             ComponentListener)))
+
+(defn painter
+  "Example painter method."
+  [^JComponent c ^Graphics2D g]
+  (.setBackground g Color/YELLOW)
+  (.clearRect g 0 0 (/ (.getWidth c) 2) (.getHeight c)))
+
+(defn frame
+  "Make a frame which draws its panel using `painter`, which is stored
+   internally and is updatable using the returned fn."
+  [painter]
+  (let [painter* (atom painter)
+        p (doto (proxy [JPanel] []
+                  (paint [g]
+                    (@painter* this ^Graphics2D g)))
+            (.setPreferredSize (Dimension. 800 600)))
+        f (doto (JFrame.)
+            (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE)
+            (.add p)
+            (.pack)
+            (.setLocationRelativeTo nil)
+            (.addWindowStateListener
+              (reify WindowStateListener
+                (windowStateChanged [this e]
+                  (condp = e
+                    WindowEvent/WINDOW_CLOSING
+                    (remove-watch painter* :refresh)
+                    nil))))
+            (.setVisible true)
+            (.setResizable true)
+            #_(.addComponentListener
+              (reify ComponentListener
+                (componentResized [self e]
+                  #_(println "resized" e)
+                  #_(println (.getWidth (first (.getComponents (.getComponent e)))))
+                  )
+                (componentMoved [self e])
+                (componentShown [self e])
+                (componentHidden [self e]))))]
+    (fn reset-painter [painter]
+      (reset! painter* painter)
+      (.update f (.getGraphics f)))))
+
+(defmacro repl-frame
+  "Make a frame which draws its panel using `painter`, which is a symbol representing
+   a var containing a fn. The frame redraws when the var is redefined."
+  [painter]
+  `(let [ret# (frame ~painter)]
+     (add-watch #'~painter :refresh
+       (fn ~'watch-painter ~'[k r o n]
+         ~'(println "watching" r)
+         (ret# ~painter)))
+     (fn ~'unwatch-painter ~'[]
+       (println "unwatching" #'~painter)
+       (remove-watch #'~painter :refresh))))
+
+(comment
+  (macroexpand-1 '(repl-frame hmm))
+  ;; => (clojure.core/let
+  ;;     [ret__65896__auto__ (comfort.ui/frame hmm)]
+  ;;     (clojure.core/add-watch
+  ;;      #'hmm
+  ;;      :refresh
+  ;;      (clojure.core/fn [k r o n] (println k r) (ret__65896__auto__ hmm)))
+  ;;     ret__65896__auto__)
+  (def rethmm (repl-frame painter))
+  (rethmm)
+  )
