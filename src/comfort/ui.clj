c16bc1565ad71d5917458f9d0117215d04bd46ad
Jeremy Field <jdf@jdf.id.au>
2024-07-01 11:07:39 +1000

    buffer painting for smooth scrolling

--- src/comfort/ui.clj
+++ src/comfort/ui.clj
@@ -5,7 +5,13 @@
   (:import (java.time LocalDate LocalDateTime)
            (java.time.temporal ChronoUnit)
            (java.time.format DateTimeFormatter)
-           (javax.swing JFrame JPanel JComponent JScrollPane)
+           (javax.swing
+             JFrame
+             JPanel
+             JLabel
+             JComponent
+             JScrollPane
+             ImageIcon)
            (java.awt
              Frame
              Graphics Graphics2D
@@ -23,23 +29,48 @@
 (defn painter
   "Example painter method."
   [^JComponent c ^Graphics2D g]
-  ;; Can (.setPreferredSize c (Dimension. <width> <height)) for JScrollPane's benefit.
+  ;; Can (.setPreferredSize c (Dimension. <width> <height)) then (.revalidate c)
+  ;; for JScrollPane's benefit. Also see `resize`.
   (.setBackground g Color/YELLOW)
   (.clearRect g 0 0 (/ (.getWidth c) 2) (.getHeight c)))
 
+(defn buffer
+  "Buffer painting into image for smooth scrolling etc."
+  [panel]
+  (let [d (.getPreferredSize panel)
+        w (.getWidth d)
+        h (.getHeight d)
+        bi (BufferedImage. w h BufferedImage/TYPE_INT_RGB)
+        g (.createGraphics bi)]
+    (.setRenderingHint g RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
+    (.setBackground g Color/WHITE)
+    (.clearRect g 0 0 w h)
+    (.paint panel g)
+    bi))
+
 (defn frame
   "Make a frame which draws its panel using `painter` (within a scroll frame),
-  which is stored internally and is updatable using the returned fn.
-  Scrolling is unoptimised; fewer redraws when dragging thumb vs gesture/wheel."
+  which is stored internally and is updatable using the returned fn."
   [painter]
   (let [painter* (atom painter)
         p (doto (proxy [JPanel] []
                   (paint [g]
+                    #_(println "painting" (LocalDateTime/now))
+                    (proxy-super paintComponent g)
                     (@painter* this ^Graphics2D g)))
             (.setPreferredSize (Dimension. 800 600)))
+        buffer* (atom (buffer p))
+        bp (doto (proxy [JPanel] []
+                   (paint [g]
+                     (proxy-super paintComponent g)
+                     (.drawImage g @buffer* 0 0 this)))
+             (.setPreferredSize (.getPreferredSize p)))
+        s (JScrollPane. bp #_p)
+        ;;hsb (doto (.getHorizontalScrollBar s) (.setUnitIncrement 100))
+        ;;vsb (doto (.getVerticalScrollBar s) (.setUnitIncrement 100))
         f (doto (JFrame.)
             (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE)
-            (.add (JScrollPane. p))
+            (.add s)
             (.pack)
             (.setLocationRelativeTo nil)
             #_(.addWindowStateListener
@@ -62,15 +93,13 @@
     {:reset-painter
      (fn reset-painter [painter]
        (reset! painter* painter)
-       ;; FIXME Only seems to repaint the first frame if multiple frames use same painter.
-       ;; (But why would anyone want that?)
-       ;; (Subsqeuent repaints work.)
-       (.update f (.getGraphics f)))
-     :repaint (fn repaint [] (.update f (.getGraphics f)))
+       (reset! buffer* (buffer p)))
+     :repaint (fn repaint [] (reset! buffer* (buffer p)))
      :frame f
      :panel p}))
 
 (defn resize
+  "Reset panel's preferred size and resize window accordingly."
   [{:keys [panel frame]} w h]
   (let [d (Dimension. w h)
         _ (.setPreferredSize panel d)
@@ -78,7 +107,8 @@
     (when-not (= d d')
       (println "Failed to setPreferredSize" d d')
       (println "min and max: " (.getMinimumSize panel) (.getMaximumSize panel))))
-  (.pack frame))
+  (.revalidate panel) ; tell JScrollPane
+  (.pack frame)) ; resize window
 
 (defn save-png
   [{:keys [panel]} filename]
@@ -95,7 +125,7 @@
     (ImageIO/write bi "png" (io/file filename))))
 
 (defmacro repl-frame
-  "Make a frame which draws its panel using `painter`, which is a symbol representing
+  "Make a frame which draws its panel using `painter`, which is a symbol bound to
    a var containing a fn. The frame redraws when the var is redefined."
   [painter]
   `(let [f# (frame ~painter)
